8920addcf1e1bd828a0dd328ce298ec9c00f5ed2
Yoan Mollard <ymollard@users.noreply.github.com>
2019-04-22 18:21:11 +0200

    Added numpy as a dep

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup()
 d['packages'] = ['edo']
 d['package_dir'] = {'': 'src'}
-d['requires'] = ['getkey']
+d['requires'] = ['getkey', 'numpy']
 # TODO Make catkin_make able to fetch getkey by itself?
 setup(**d)
